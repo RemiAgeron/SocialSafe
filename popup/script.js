8c9fdc28e7a2d16aefd2a07cd20fb43d2376e83b
RemiAgeron <remiageron@gmail.com>
2024-10-15 15:50:01 +0200

    Update popup action add authentication form to access admin mode

--- popup/script.js
+++ popup/script.js
@@ -2,15 +2,38 @@ async function hashPassword(password) {
   const msgUint8 = new TextEncoder().encode(password);
   const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-  return hashHex;
+  return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
 }
 
-async function storePassword(password) {
-  const hashedPassword = await hashPassword(password);
-  chrome.storage.local.set({ adminPassword: hashedPassword }, function () {
+async function storePassword(hashedPassword) {
+  chrome.storage.local.set({ adminPassword: hashedPassword }, () => {
+    document.getElementById("adminLogin").hidden = true;
   });
 }
+
+async function verifyPassword(password) {
+  const hashedPassword = await hashPassword(password);
+  return new Promise((resolve) => {
+    chrome.storage.local.get(['adminPassword'], (result) => {
+      if (result.adminPassword) {
+        resolve(result.adminPassword === hashedPassword);
+      } else {
+        resolve(storePassword(hashedPassword));
+      }
+    });
+  });
+}
+
+document.getElementById('loginForm')?.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const password = document.getElementById('password').value;
+  const isPasswordCorrect = await verifyPassword(password);
+  if (isPasswordCorrect) {
+    window.location.href = 'actions-admin.html';
+  } else {
+    document.getElementById('errorMessage').hidden = false;
+  }
+});
 
 document.getElementById("navActions")?.addEventListener("click", () => {
   window.location.href = 'actions.html';
@@ -20,11 +43,24 @@ document.getElementById("navStats")?.addEventListener("click", () => {
   window.location.href = 'stats.html';
 })
 
-document.getElementById("actionAdmin")?.addEventListener("click", () => {
-  const admin = document.getElementById("admin");
-  if (admin !== undefined) {
-    admin.hidden = !admin.hidden;
-  }
+document.getElementById("navActionsAdmin")?.addEventListener("click", () => {
+  window.location.href = 'actions-admin.html';
+})
+
+document.getElementById("navStatsAdmin")?.addEventListener("click", () => {
+  window.location.href = 'stats-admin.html';
+})
+
+document.getElementById("actionAdmin")?.addEventListener("click", async () => {
+  chrome.storage.local.get(["adminPassword"], (result) => {
+    if (!result.adminPassword) document.getElementById('creationMessage').hidden = false;
+  });
+  const adminLogin = document.getElementById("adminLogin");
+  if (adminLogin !== undefined) adminLogin.hidden = !adminLogin.hidden;
+})
+
+document.getElementById("actionQuit")?.addEventListener("click", () => {
+  window.location.href = 'actions.html';
 })
 
 document.getElementById("actionSearch")?.addEventListener("click", () => {
